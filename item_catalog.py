db72e3cc7b7c5e8e5b261f304cb44f52773fa1df
Jim Skibbie <jaskibbie@gmail.com>
2015-09-19 18:34:13 -0500

    Begin work on showCatalog function.

--- item_catalog.py
+++ item_catalog.py
@@ -3,22 +3,18 @@
 import os
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, session
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker, exc
-from catalog import Base, Shelter, Puppy, Profile, Adopter
-from puppies import *
-#from flask.ext.sqlalchemy import SQLAlchemy
-import datetime
-import sys
+from catalog_database_setup import Base, Category, Item
 
 
-engine = create_engine('sqlite:///puppyshelter.db')
+engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.bind = engine
  
 DBSession = sessionmaker(bind=engine)
 
-session = DBSession()
+db_session = DBSession()
 
 app = Flask(__name__)
 
@@ -26,32 +22,36 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/catalog/')
 def showCatalog():
-    return "This page will show all categories and the top ten latest items."
+    categories = db_session.query(Category).order_by(asc(Category.name))
+    last_items = db_session.query(Item, Category).filter(Item.id==Category.id).order_by(desc(Item.last_updated)).limit(10)
+    return render_template('catalog.html', categories=categories, last_items=last_items )
 
 """This route shows all items for a particular category."""
 @app.route('/catalog/<string:category_name>/items/')
 def showItems(category_name):
-    return "This page will show all items for a specific category name."
+    return "This page will show all items for the category: %s." % category_name
 
 """This route shows details about a single item."""
 @app.route('/catalog/<string:category_name>/<string:item_name>/')
 def showItem(category_name, item_name):
-    return "This page will show details about a single item."
+    return "This page will show details about the item %s for category: %s." % (item_name, category_name)
 
 """This route allows a user to create a new item for a given category."""
 @app.route('/catalog/<string:category_name>/new/')
 def createItem(category_name):
-    return "This page will allow the user to create a new item for a given category."
+    # if item_name.lower() == 'items':
+    #     return "Items is a reserved word for this application. You cannot use it as the name of an item."
+    return "This page will allow the user to create a new item for the category: %s" % category_name
 
 """This route allows a user to edit a specific item they created."""
-@app.route('/catalog/<string:item_name>/edit')
+@app.route('/catalog/<string:item_name>/edit/')
 def editItem(item_name):
-    return "This page will allow the user to edit a specific item."
+    return "This page will allow the user to edit a the item: '%s'" % item_name
 
 """This route allows a user to delete a specific item they created."""
 @app.route('/catalog/<string:item_name>/delete')
 def deleteItem(item_name):
-    return "This page will allow the user to delete a specific item they created."
+    return "This page will allow the user to delete the item: %s" % item_name
 
 """This route handles logging in the user using the Google OAuth API."""
 @app.route('/catalog/login/')
