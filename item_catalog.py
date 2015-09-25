9e3d9cb60185c6f802d4c70c046ec58d51dcbe2c
Jim Skibbie <jaskibbie@gmail.com>
2015-09-24 21:24:14 -0500

    More work building out route functions.

--- item_catalog.py
+++ item_catalog.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, redirect, jsonify, url_for, f
 
 from flask_wtf.csrf import CsrfProtect
 # from wtf_form_functions import MyForm
-from wtf_form_functions import createItemForm
+from wtf_form_functions import createItemForm, editItemForm
 
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker, exc
@@ -49,16 +49,6 @@ def register():
 ### testing flask_wtf
 
 
-
-#Fake categories
-categories = [{'name': 'Soccer', 'id': '1'}, {'name': 'Basketball', 'id': '2'}, {'name': 'Baseball', 'id': '3'}, {'name': 'Frisbee', 'id': '4'}, {'name': 'Snowboarding', 'id': '5'}, {'name': 'Rock Climbing', 'id': '6'}, {'name': 'Foosball', 'id': '7'}, {'name': 'Skating', 'id': '8'}, {'name': 'Hockey', 'id': '9'}]
-category = {'name': 'Snowboarding', 'id': '5'}
-
-# items = [{'id': '1', 'name': 'Stick', 'description': 'Used to hit the puck.', 'image_url': 'http://example.com/stick.png', 'last_updated': '2015-09-19 10:28:00', 'category_id': '9'}, {'id': '2', 'name': 'Goggles', 'description': 'Used to protect your eyes.', 'image_url': 'http://example.com/goggles.png', 'last_updated': '2015-09-20 09:27:00', 'category_id': '5'}, {'id': '3', 'name': 'Snowboard', 'description': 'Used to travel down the mountain.', 'image_url': 'http://example.com/snowboard.png', 'last_updated': '2015-09-18 10:27:00', 'category_id': '5'}, {'id': '3', 'name': 'Shinguards', 'description': 'Keeps shins safe from kicks.', 'image_url': 'http://example.com/shinguard.png', 'last_updated': '2015-09-16 07:14:00', 'category_id': '1'}]
-items = [{'id': '2', 'name': 'Goggles', 'description': 'Used to protect your eyes.', 'image_url': 'http://example.com/goggles.png', 'last_updated': '2015-09-20 09:27:00', 'category_id': '5'}, {'id': '3', 'name': 'Snowboard', 'description': 'Used to travel down the mountain.', 'image_url': 'http://example.com/snowboard.png', 'last_updated': '2015-09-18 10:27:00', 'category_id': '5'}]
-item = {'id': '2', 'name': 'Goggles', 'description': 'Used to protect your eyes.', 'image_url': 'http://example.com/goggles.png', 'last_updated': '2015-09-20 09:27:00', 'category_id': '5'}
-
-
 """This route is the root of the web application and returns the catalog categories and top ten recent items."""
 @app.route('/')
 @app.route('/catalog/')
@@ -70,40 +60,76 @@ def showCatalog():
 """This route shows all items for a particular category."""
 @app.route('/catalog/<string:category_name>/items/')
 def showCategory(category_name):
-    return render_template('showCategory.html', category=category, items=items, item_count=str(len(items)) )
+    categories = db_session.query(Category).order_by(asc(Category.name))
+    category = db_session.query(Category).filter_by(name=category_name).one()
+    items = db_session.query(Item, Category).filter(Item.category_id==Category.id).filter_by(category_id=category.id)
+
+    return render_template('showCategory.html', categories = categories, category_name=category_name, items=items, item_count=items.count() )
 
 """This route allows a user to create a new item for a given category."""
-@app.route('/catalog/<string:category_name>/new/')
-def createItem(category_name, methods=('GET', 'POST')):
+@app.route('/catalog/<string:category_name>/new/', methods=('GET', 'POST'))
+def createItem(category_name):
     # if item_name.lower() == 'items':
     #     return "Items is a reserved word for this application. You cannot use it as the name of an item."
     categories = db_session.query(Category).order_by(asc(Category.name))
-    form = createItemForm(request.form)
+    category = db_session.query(Category).filter_by(name=category_name).one()
+    form = createItemForm(request.form, category=category.id)
     form.category.choices =  [(c.id, c.name) for c in categories]
     
     if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        #             form.password.data)
-        # db_session.add(user)
-        flash('Your new item has been entered')
-        return redirect(url_for('showCategory'))
-    return render_template('createItem.html', form=form)
-    #return "This page will allow the user to create a new item for the category: %s" % category_name
+        newItem = Item( name=request.form['name'],
+                        description = request.form['description'],
+                        image_url = request.form['image_url'],
+                        category_id = request.form['category'])
+        db_session.add(newItem)
+        category = db_session.query(Category).filter_by(id=request.form['category']).one()
+        flash('New item: %s has been created.' % newItem.name)
+        return redirect(url_for('showCategory', category_name=category.name))
+    return render_template('createItem.html', form=form, category_name=category_name)
 
 """This route allows a user to edit a specific item they created."""
-@app.route('/catalog/<string:item_name>/edit/')
+@app.route('/catalog/<string:item_name>/edit/', methods=['GET', 'POST'])
 def editItem(item_name):
-    return "This page will allow the user to edit the item: '%s'" % item_name
+    categories = db_session.query(Category).order_by(asc(Category.name))    
+    #item = db_session.query(Item, Category).filter(Item.category_id==Category.id).filter_by(name=item_name).one()
+    item = db_session.query(Item).filter_by(name=item_name).one()
+    category = db_session.query(Category).filter_by(id=item.category_id).one()
+    form = editItemForm(request.form,
+                        name=item.name,
+                        description=item.description,
+                        image_url=item.image_url,
+                        category=item.category_id)
+    form.category.choices =  [(c.id, c.name) for c in categories]
+    
+    if request.method == 'POST' and form.validate():
+        item = db_session.query(Item).filter_by(name=item_name).one()
+        item.name = request.form['name']
+        item.description = request.form['description']
+        item.image_url = request.form['image_url']
+        item.category_id = request.form['category']
+        db_session.add(item)
+        db_session.commit()
+        category = db_session.query(Category).filter_by(id=request.form['category']).one()
+        return redirect(url_for('showCategory', category_name=category.name))
+    return render_template('editItem.html', form=form, item_name=item_name, category_name=category.name)
 
 """This route allows a user to delete a specific item they created."""
-@app.route('/catalog/<string:item_name>/delete/')
+@app.route('/catalog/<string:item_name>/delete/', methods=['GET', 'POST'])
 def deleteItem(item_name):
-    return "This page will allow the user to delete the item: %s" % item_name
+    item = db_session.query(Item).filter_by(name=item_name).one()
+    category = db_session.query(Category).filter_by(id=item.category_id).one()
+
+    if request.method == 'POST':
+        db_session.delete(item)
+        flash('Your item has been deleted.')
+        return redirect(url_for('showCategory', category_name=category.name))
+    return render_template('deleteItem.html', item_name=item_name, category_name=category.name)
 
 """This route shows details about a single item."""
-@app.route('/catalog/<string:category_name>/<string:item_name>/')
+@app.route('/catalog/<string:category_name>/<string:item_name>/', methods=['GET'])
 def showItem(category_name, item_name):
-    return render_template('showItem.html', item=item)
+    item = db_session.query(Item).filter_by(name=item_name).one()
+    return render_template('showItem.html', item=item, category_name=category_name)
 
 """This route handles logging in the user using the Google OAuth API."""
 @app.route('/catalog/login/')
